8b70bfb3562ff879c8bd80f830c61c1584011990
self-unit <self_unit@hotmail.com>
2018-08-27 15:44:20 +0100

    view restructure for selection

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require('sinatra')
 require('sinatra/contrib/all')
 require_relative('/controllers/visitors_controller.rb')
 require_relative('/controllers/managers_controller.rb')
+require_relative('/controllers/artist_controller.rb')
+require_relative('/controllers/exhibit_controller.rb')
 
 get '/' do
   erb( :index )
--- controllers/managers_controller.rb
+++ controllers/managers_controller.rb
@@ -5,22 +5,95 @@ require_relative('../models/artist.rb')
 require_relative('../models/exhibit.rb')
 also_reload('../models/*')
 
+#HOME
+get '/managers' do
+  erb( :home )
+end
+
+#INDEX OF ARTISTS
 get '/artists' do
-  erb(:)
+  @artists = Artist.all
+  erb( :index )
 end
 
-get '/artists/:id' do
-  erb(:)
-end
-
+#NEW ARTIST
 get '/artists/new' do
-  erb(:)
+  erb( :new )
 end
 
-get '/artists/:id/edit' do
-  erb(:)
+#CREATE ARTIST
+post '/artists' do
+  create_artist = Artist.new(params)
+  create_artist.save()
+  redirect to '/artists'
 end
 
+#SHOW ARTIST
+get '/artists/:id' do
+  @artist = Artist.find(params['id'].to_i)
+  erb( :show )
+end
+
+#EDIT ARTIST
 get '/artists/:id/edit' do
-  erb(:)
+  @artist = Artist.find(params['id'].to_i)
+  erb( :edit )
+end
+
+#UPDATE ARTIST
+post '/artists/:id' do
+  update_artist = Artist.new(params)
+  update_artist.update()
+  redirect to '/artists'
+end
+
+#DELETE ARTIST
+delete '/artists/:id/delete' do
+  delete_artist = Artist.find(params['id'].to_i)
+  delete_artist.delete()
+  redirect to '/artists'
+end
+
+#INDEX OF EXHIBITS
+get '/exhibits' do
+  @exhibits = Exhibit.all
+  erb( :index )
+end
+
+#SHOW EXHIBIT
+get '/exhibits/:id' do
+  @exhibit = Exhibit.find(params['id'].to_i)
+  erb( :show )
+end
+
+#NEW EXHIBIT
+get '/exhibits/new' do
+  erb( :new )
+end
+
+#CREATE ARTIST
+post '/exhibits' do
+  create_exhibit = Exhibit.new(params)
+  create_exhibit.save()
+  redirect to '/exhibits'
+end
+
+#EDIT EXHIBIT
+get '/exhibits/:id/edit' do
+  @exhibit = Exhibit.find(params['id'].to_i)
+  erb( :edit )
+end
+
+#UPDATE EXHIBIT
+post '/exhibits/:id' do
+  update_exhibit = Exhibit.new(params)
+  update_exhibit.update()
+  redirect to '/exhibits'
+end
+
+#DELETE EXHIBIT
+delete '/exhibits/:id/delete' do
+  delete_exhibit = Exhibit.find(params['id'].to_i)
+  delete_exhibit.delete()
+  redirect to '/exhibits'
 end
