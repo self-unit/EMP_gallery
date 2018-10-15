295dc009cebb61a060456c0aad568e5d68569af3
self-unit <self_unit@hotmail.com>
2018-10-15 13:20:51 +0100

    final prep for heroku

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -4,7 +4,8 @@ class SqlRunner
 
   def self.run(sql, values = [])
     begin
-      db = PG.connect({ dbname: 'gallery', host: 'localhost' })
+      # db = PG.connect({ dbname: 'gallery', host: 'localhost' })
+      db = PG.connect( {dbname: 'dc541e3uel6roq', host: 'ec2-23-21-147-71.compute-1.amazonaws.com', port: 5432, user: 'lgyeaeowktbife', password: '3f592c5e64f08e93d7f8c59a2f94dd2c3dd74bd514ad10dfdd2fcf80c853b794'})
       db.prepare("query", sql)
       result = db.exec_prepared("query", values)
     ensure
